649d345793143148245777993e303a06c2c0a6f5
Wish-baek <dnjs0236@gmail.com>
2021-04-01 21:35:37 +0900

    while 문 활용

---
+++ CalSum/src/CalSum.java
@@ -0,0 +1,17 @@
+
+public class CalSum {
+
+	public static void main(String[] args) {
+
+		int i = 1 ;
+		int sum = 0 ;
+		
+		while ( i < 11 ) {
+			
+			sum = sum +i;
+			i++;
+		}
+		System.out.println("гу╟Х="+sum);
+	}
+
+}
