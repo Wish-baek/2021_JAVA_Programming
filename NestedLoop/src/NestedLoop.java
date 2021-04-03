f98e3371fe609ef19ca21a1f66e683f11885b125
Wish-baek <dnjs0236@gmail.com>
2021-04-03 21:01:59 +0900

    중첩 반복문 활용 구구단 출력

---
+++ NestedLoop/src/NestedLoop.java
@@ -0,0 +1,14 @@
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+
+		for (int i = 2; i < 10; i++) {
+			for (int n = 1; n < 10; n++) {
+				System.out.println(i + "*" + n + "=" + i*n);
+			}
+		}
+		
+	}
+
+}
