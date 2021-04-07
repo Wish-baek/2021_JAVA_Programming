0d91b6a6fed1e41d36affe26321d6764dd4c5f56
Wish-baek <dnjs0236@gmail.com>
2021-04-07 21:18:50 +0900

    2개 주사위의 합이 6이 되는 경우 출력

---
+++ p3-3/src/p3.java
@@ -0,0 +1,14 @@
+
+public class p3 {
+
+	public static void main(String[] args) {
+
+		int i;
+		
+		for (i = 1; i<6 ; i++) {
+			System.out.print( "(" + i + "," + (6-i) + ") , " );
+		}
+		
+	}
+	
+}
