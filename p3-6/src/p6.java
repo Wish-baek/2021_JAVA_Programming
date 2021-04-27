12ef92174afc695e2d195427129288d54844aea7
Wish-baek <dnjs0236@gmail.com>
2021-04-27 15:32:01 +0900

    2와 100 사이의 모든 소수 출력

--- p3-6/src/p6.java
+++ p3-6/src/p6.java
@@ -2,22 +2,17 @@
 public class p6 {
 
 	public static void main(String[] args) {
-		
-		int k;
-		int x;
-		
-		System.out.print("2부터 100사이 모든 소수: ");
-		for (k=2; k<=100; k++) {
-			int c=0;
-			for (x=2; x<k; x++) {
-				if ( k % x == 0) 
-					c = c+1;
-				
-				
+		// TODO Auto-generated method stub
+
+		for (int k=2; k<100; k++) {
+			int count = 0;
+			for (int i=2; i<k; i++) {
+				if (k%i ==0)
+					count = 1;
+					break;
 			}
-			if (c <1)
+			if (count == 0)
 				System.out.print(k + " ");
-			
 		}
 		
 	}
