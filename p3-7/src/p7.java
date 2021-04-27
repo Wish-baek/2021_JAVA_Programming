cb363f21646a6f2ccdacb567c9bee89233058fe5
Wish-baek <dnjs0236@gmail.com>
2021-04-27 15:39:09 +0900

    피타고라스의 정리

--- p3-7/src/p7.java
+++ p3-7/src/p7.java
@@ -2,19 +2,17 @@
 public class p7 {
 
 	public static void main(String[] args) {
-		
-		int a;
-		int b;
-		int c;
-		
-		for (a=1; a<100; a++) {
-			for (b=1; b<100; b++) {
-				for (c=1; c<=100; c++) {
-					if (a*a+b*b==c*c)
+		// TODO Auto-generated method stub
+
+		for (int a=1; a<100; a++) {
+			for (int b=1; b<100; b++) {
+				for (int c=1; c<100; c++) {
+					if (a*a + b*b == c*c)
 						System.out.println(a + " " + b + " " + c);
 				}
 			}
 		}
+		
 	}
 
 }
